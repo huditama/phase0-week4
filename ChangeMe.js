a0b4dd8c4cb7387f102db10fbc890bd938cecdf5
Reyhan Huditama <reyhan21098@gmail.com>
2019-01-16 11:42:20 +0700

    Tugas Week 4

---
+++ ChangeMe.js
@@ -0,0 +1,32 @@
+function changeMe(arr) {
+  for (i = 0; i < arr.length; i++) {
+   
+  if (arr[i][3] === undefined) {
+    arr[i][3] = 'Invalid Birth Year'
+  } else {
+    arr[i][3] = 2019 - arr[i][3]
+  }
+
+  var object = {
+    firstName:  arr[i][0],
+    lastName: arr[i][1],
+    gender: arr[i][2],
+    age: arr[i][3]
+  }
+  console.log(object)
+ }
+}
+
+
+// TEST CASES
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 37 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
