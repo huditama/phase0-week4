8fb784426ea2f383ec8a1aa7b864191aa5622a78
Reyhan Huditama <reyhan21098@gmail.com>
2019-01-17 15:51:27 +0700

    Tugas Week 4

---
+++ TokoX.js
@@ -0,0 +1,97 @@
+function countProfit(shoppers) {
+  let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
+                     ['Baju Zoro', 500000, 2],
+                     ['Sweater Uniklooh', 175000, 1]
+                   ];
+  var temp = []
+  
+  for (i = 0; i < listBarang.length; i++) {
+    var productShopping = listBarang[i][0]
+    var priceShopping = listBarang[i][1]
+    var stockShopping = listBarang[i][2]
+
+    var obj = {
+        product: productShopping,
+        shoppers: [],
+        leftOver: stockShopping,
+        totalProfit: 0
+      }
+     
+
+    for (j = 0; j < shoppers.length; j++) {
+      var shoppersName = shoppers[j].name
+      var productMatch = shoppers[j].product
+      var totalCart = shoppers[j].amount
+    
+      if(productMatch == productShopping && stockShopping >= totalCart && obj.leftOver >= totalCart ) {
+        obj.shoppers.push(shoppersName)
+        obj.leftOver -= totalCart
+        obj.totalProfit = priceShopping * (stockShopping - obj.leftOver)
+
+      
+      }
+    }
+   temp.push(obj) 
+  }
+return (temp)
+}
+
+//TEST CASES
+console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 3}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 2}]));
+//[ { product: 'Sepatu Stacattu',
+//   shoppers: [ 'Windi', 'Vanessa' ],
+//   leftOver: 5,
+//   totalProfit: 7500000 },
+// { product: 'Baju Zoro',
+//   shoppers: [],
+//   leftOver: 2,
+//   totalProfit: 0 },
+// { product: 'Sweater Uniklooh',
+//   shoppers: [],
+//   leftOver: 1,
+//   totalProfit: 0 } ]
+
+
+console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 8}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 10}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 1}, {name: 'Devi', product: 'Baju Zoro', amount: 1}, {name: 'Lisa', product: 'Baju Zoro', amount: 1}]));
+// [ { product: 'Sepatu Stacattu',
+//     shoppers: [ 'Windi' ],
+//     leftOver: 2,
+//     totalProfit: 12000000 },
+//   { product: 'Baju Zoro',
+//     shoppers: [ 'Devi', 'Lisa' ],
+//     leftOver: 0,
+//     totalProfit: 1000000 },
+//   { product: 'Sweater Uniklooh',
+//     shoppers: [ 'Rani' ],
+//     leftOver: 0,
+//     totalProfit: 175000 } ]
+
+
+console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
+// [ { product: 'Sepatu Stacattu',
+//     shoppers: [],
+//     leftOver: 10,
+//     totalProfit: 0 },
+//   { product: 'Baju Zoro',
+//     shoppers: [],
+//     leftOver: 2,
+//     totalProfit: 0 },
+//   { product: 'Sweater Uniklooh',
+//     shoppers: [],
+//     leftOver: 1,
+//     totalProfit: 0 } ]
+
+
+console.log(countProfit([])); //[]
+//[ { product: 'Sepatu Stacattu',
+//   shoppers: [ 'Windi', 'Vanessa' ],
+//   leftOver: 5,
+//   totalProfit: 7500000 },
+// { product: 'Baju Zoro',
+//   shoppers: [],
+//   leftOver: 2,
+//   totalProfit: 0 },
+// { product: 'Sweater Uniklooh',
+//   shoppers: [],
+//   leftOver: 1,
+//   totalProfit: 0 } ]
